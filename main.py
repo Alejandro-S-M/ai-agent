9cbd600790c990a5761dc49d7f419031fc6358be
Alejandro-S-M <alejandro.sanchez.mejorada@gmail.com>
2025-08-26 11:57:55 -0400

    Integrate Google's Gemini API

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+# python
+
+import os
+from google import genai
+from dotenv import load_dotenv, dotenv_values
+
 def main():
     print("Hello from ai-agent!")
-
+    ok = load_dotenv()  # or load_dotenv(dotenv_path=".env")
+    print("Setup status:")
+    print("dotenv loaded:", ok)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    print("API key present:", bool(api_key))
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+    print("Prompt tokens:",  response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
