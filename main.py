5f7310ba83a8a9e3f913a2cbf04c058d7c9bfe00
Alejandro-S-M <alejandro.sanchez.mejorada@gmail.com>
2025-08-28 12:03:43 -0400

    Add verbose flag check and output response, add list compresion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# python
+# ai-agent
 
 import os
 import sys
@@ -9,34 +9,38 @@ from google.genai import types
 def main():
     print("Hello from ai-agent!")
     ok = load_dotenv() 
-    print("Setup status:")
-    print("dotenv loaded:", ok)
+    print("Setup status:", "dotenv loaded =", ok)
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     print("API key present:", bool(api_key))
     client = genai.Client(api_key=api_key)
     
-    # Get user input
-    user_prompt = input("Enter a prompt: ")
+    # User input with sys.argv
+    args = sys.argv[1:]
+    verbose_flag = "--verbose" in args
+    prompt_args = [arg for arg in args if arg != "--verbose"]
     
-    # Validate input
-    if not user_prompt:
+    # Check for verbose flag and uotuput reponse
+    if not prompt_args:
         print("Error: prompt not provided")
-        exit()    
-    
-    # Create messages list
-    messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-    ]
+        return
 
-    # Output response
+    user_prompt = prompt_args[0]
+
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=messages, 
+        contents=messages,
     )
-    print("Prompt tokens:",  response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
+
+    if verbose_flag:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", prompt_tokens)
+        print("Response tokens:", response_tokens)
+
     print(response.text)
 
 if __name__ == "__main__":
